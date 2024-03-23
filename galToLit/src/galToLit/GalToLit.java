ac342f3203d7badebc57857547de4b80317c5a16
FerreiraLiraLaura <lauraferreiralira@gmail.com>
2024-03-23 14:54:08 -0300

    exercício 3, JF 4-1

--- galToLit/src/galToLit/GalToLit.java
+++ galToLit/src/galToLit/GalToLit.java
@@ -7,19 +7,21 @@
 	public static void main(String[] args) {
 	// declare as variáveis
 		Scanner scanner = new Scanner(System.in);
-	double gallons=10;
+	double gallons;
 	double liters=3.785;
-	int galoes;
+	
 	
 	// adicione o cálculo aqui
 	
 	// exiba o resultado para o usuário
-	System.out.println(gallons+" gallonsequals "+liters+" liters");
+	System.out.println("Litro por galão:" +liters++);
 	System.out.println("Quantos galões desejas adquirir?");
-	 galoes = scanner.nextInt();
-	 double gallonsequals = (galoes*liters);
+	 gallons = scanner.nextDouble();
+	 double gallonsequals = (gallons*liters);
 	
 	System.out.println(+gallonsequals++);
+	
+
 	}
 	
 }
