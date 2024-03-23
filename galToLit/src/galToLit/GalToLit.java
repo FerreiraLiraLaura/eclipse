b6a9df2d84bb3c2ab5fb07ca32d76a647afcb44a
FerreiraLiraLaura <lauraferreiralira@gmail.com>
2024-03-23 14:37:03 -0300

    calculo

--- galToLit/src/galToLit/GalToLit.java
+++ galToLit/src/galToLit/GalToLit.java
@@ -1,12 +1,25 @@
 
-package galToLit;
-public class GalToLit {
-public static void main(String[] args) {
-// declare as variáveis
-double gallons=10;
-double liters=0;
-// adicione o cálculo aqui
-// exiba o resultado para o usuário
-System.out.println(gallons+" gallons equals "+liters+" liters");
-}
+	package galToLit;
+	import java.util.Scanner;
+	
+	public class GalToLit {
+		
+	public static void main(String[] args) {
+	// declare as variáveis
+		Scanner scanner = new Scanner(System.in);
+	double gallons=10;
+	double liters=3.785;
+	int galoes = 0;
+	double gallonsequals = (galoes*liters);
+	// adicione o cálculo aqui
+	
+	// exiba o resultado para o usuário
+	System.out.println(gallons+" gallonsequals "+liters+" liters");
+	System.out.println("Quantos galões desejas adquirir?");
+	 galoes = scanner.nextInt();
+	
+	
+	System.out.println(+gallonsequals++);
+	}
+	
 }
